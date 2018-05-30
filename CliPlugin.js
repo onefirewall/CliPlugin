40973fd0026fc70b914d4d15bad127e1a1f31884
justPassingBy1 <37617149+justPassingBy1@users.noreply.github.com>
2018-05-30 15:29:47 +0200

    added some methods (untested)

--- CliPlugin.js
+++ CliPlugin.js
@@ -1,3 +1,5 @@
+IP_REGEX = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$|^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$|^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$/g;
+
 var SSH = require('simple-ssh');
 
 var CliPlugin = function (host, user, psw) {
@@ -18,6 +20,54 @@ var CliPlugin = function (host, user, psw) {
 
 function createLoginWithSSH(ssh) {
     ssh.exec('echo command', {
+        out: function(out) {
+            console.log(out);
+        }
+    }).start();
+}
+
+function getRootAccess(ssh) {
+    ssh.exec('enable', {
+        out: function(out) {
+            console.log(out);
+        }
+    }).start();
+}
+
+function getConfigAccess(ssh) {
+    ssh.exec('configure terminal', {
+        out: function(out) {
+            console.log(out);
+        }
+    }).start();
+}
+
+function blockIPs(ssh, ipList) {
+    for (i = 0; i < ipList.length; i++) {
+        if(ipList[i].match(IP_REGEX)) {
+            ssh.exec('access-list 200 deny ipList[i]', {
+            out: function(out) {
+                    console.log(out);
+                }
+            }).start();
+        }
+    }
+}
+
+function enableIPs(ssh, ipList) {
+    for (i = 0; i < ipList.length; i++) {
+        if(ipList[i].match(IP_REGEX)) {
+            ssh.exec('access-list 200 permit ipList[i]', {
+            out: function(out) {
+                    console.log(out);
+                }
+            }).start();
+        }
+    }
+}
+
+function exit(ssh, ipList) {
+    ssh.exec('exit', {
         out: function(out) {
             console.log(out);
         }
