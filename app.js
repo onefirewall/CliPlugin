11cc8022f21fca00a32e9d6c28199e789f83a9cc
justPassingBy1 <37617149+justPassingBy1@users.noreply.github.com>
2018-06-24 18:04:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,15 +16,19 @@ var ifc = properties.get('interface');
 var CliPlugin = require('./CliPlugin.js');
 //var cliPluginCommand = new cliPlugin(host, user, psw);
 var cliPluginCommand = new CliPlugin();
+cliPluginCommand.mainApp(host, 0 , ifc);
 
-cliPluginCommand.mainApp(host, 0, ifc ,function callback(jsonArray) {
-    console.log(JSON.stringify(jsonArray[0]));
-});
+//INIT
+//cliPluginCommand.mainApp(host, 0, ifc ,function callback(jsonArray) {
+ //   console.log(JSON.stringify(jsonArray[0]));
+//});
 
-cliPluginCommand.mainApp(host, 1, ipList, function callback(jsonArray) {
-    console.log(JSON.stringify(jsonArray[0]));
-});
+//ADD
+//cliPluginCommand.mainApp(host, 1, ipList, function callback(jsonArray) {
+ //   console.log(JSON.stringify(jsonArray[0]));
+//});
 
-cliPluginCommand.mainApp(host, 2, ipList, function callback(jsonArray) {
-    console.log(JSON.stringify(jsonArray[0]));
-});
+//DELETE
+//cliPluginCommand.mainApp(host, 2, ipList, function callback(jsonArray) {
+ //   console.log(JSON.stringify(jsonArray[0]));
+//});
