078b8e02e2080940a1c28338c161960f4e390908
justPassingBy1 <37617149+justPassingBy1@users.noreply.github.com>
2018-07-03 22:13:22 +0200

    Update CliPlugin.js

--- CliPlugin.js
+++ CliPlugin.js
@@ -50,14 +50,12 @@ var connectViaSSH = function(host, user, psw, opsType, ipList, ifc, port, callba
         //var arrayOfCommandsAdd = ["conf t", "no ip access-group " + accessListName + " in", "end", "conf t", "ip access-list standard "+ accessListName, "no permit any", "permit any", "end", "conf t", "ip access-group " + accessListName + " in", "end"];
         console.log("ADD operation, parsing ip list");
         
-        var addIpsCommand = "ios_config \"ip access-list standard "+ accessListName + " \" \" no permit any \" \" permit any \" ";
-        var i=0;
-        //TODO insert in addIpsCommand the denies between the permit (splice is used for arrays)
-        while(i<ipList.length) {
-                addIpsCommand.splice(7+i,0,"deny "+ipList[i]);
-                i++
+        var addIpsCommand = "ios_config \"ip access-list standard "+ accessListName + " \" \" no permit any \" \" \" \" permit any \" ";
+        for(var i = 0 ; i<ipList.length; i++) {
+                addIpsCommand=addIpsCommand.replace("\" \" \" \"","\" \"deny "+ipList[i]+" \" \" \" \"");
         }
-        
+        addIpsCommand = addIpsCommand.replace( "\" \" \" \"" , "\" \"" );
+ 
         var arrayOfCommandsAdd = ["tclsh", "ios_config \"interface "+ifc+" \" \"no ip access-group " + accessListName + " in \" ", addIpsCommand, " ios_config \"interface "+ifc+ " \" \" ip access-group " + accessListName + " in \" ", "exit"];
 
         listOfCommands = arrayOfCommandsAdd.slice(0,arrayOfCommandsAdd.length)
@@ -65,19 +63,19 @@ var connectViaSSH = function(host, user, psw, opsType, ipList, ifc, port, callba
         break;
       
       case 2:
-        var arrayOfCommandsDelete = ["conf t", "ip access-list standard "+ accessListName, "end"];
-        
+        //var arrayOfCommandsDelete = ["conf t", "ip access-list standard "+ accessListName, "end"];
         console.log("DELETE operation, parsing ip list");
-        var i=0;
-        while (i<ipList.length) {
-                arrayOfCommandsDelete.splice(2+i,0,"no deny "+ipList[i]);
-                i++;
+        
+        var deleteIpsCommand = "ios_config \"ip access-list standard "+ accessListName + " \" \" no permit any \" \" \" \" permit any \" ";
+        for(var i = 0 ; i<ipList.length; i++) {
+                deleteIpsCommand=deleteIpsCommand.replace("\" \" \" \"","\" \"no deny "+ipList[i]+" \" \" \" \"");
         }
-        listOfCommands = arrayOfCommandsDelete.slice(0,arrayOfCommandsDelete.length)
+        deleteIpsCommand = deleteIpsCommand.replace( "\" \" \" \"" , "\" \"" );
+        
+        listOfCommands = deleteIpsCommand.slice(0,deleteIpsCommand.length)
         break;
         
       case 3:
-        //TOCHECK does this de-associate the access-list?
         var arrayOfCommandsClear = ["conf t", "interface " + ifc , "no ip access-group "+ accessListName + " in", "end", "conf t", "no ip access-list standard "+ accessListName, "end"];
 
         console.log("CLEAR operation");
