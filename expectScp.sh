d48703741bcfb43828e92a925102553ad002eebe
justPassingBy1 <37617149+justPassingBy1@users.noreply.github.com>
2018-07-14 17:40:26 +0200

    Update expectScp.sh

--- expectScp.sh
+++ expectScp.sh
@@ -4,7 +4,8 @@ set port [lindex $argv 0]
 set commandFile [lindex $argv 1]
 set user [lindex $argv 2]
 set host [lindex $argv 3]
-set password [lindex $argv 4]
+set mountPoint [lindex $argv 4]
+set password [lindex $argv 5]
 
 spawn scp -P "$port" ./"$commandFile" "$user"@"$host":"$mountPoint":"$commandFile"
 expect "*?assword:*"
